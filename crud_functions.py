a928f1798df29c888085d8508d642a3cef6738e4
UlanovVV <vovancux39@gmail.com>
2024-10-25 14:19:17 +0200

    Add files via upload

---
+++ crud_functions.py
@@ -0,0 +1,32 @@
+import sqlite3
+
+
+def initiate_db():
+    connection = sqlite3.connect('Products.db')
+    cursor = connection.cursor()
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Products(
+    id INTEGER PRIMARY KEY,
+    title TEXT NOT NULL,
+    description TEXT,
+    price INTEGER NOT NULL
+    );
+    ''')
+    cursor.execute('DELETE FROM Products')
+
+    for i in range(1, 5):
+        cursor.execute('INSERT INTO Products (title, description, price) VALUES (?, ?, ?)',
+                       (f'Продукт {i}', f'Описание {i}', f'{i*100}',))
+
+    connection.commit()
+    connection.close()
+
+
+def get_all_products():
+    connection = sqlite3.connect('Products.db')
+    cursor = connection.cursor()
+    cursor.execute('SELECT * FROM Products')
+    db = cursor.fetchall()
+    connection.commit()
+    connection.close()
+    return list(db)
