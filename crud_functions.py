d33b2aa96f0268b6d704032896fe308ae92a16fd
UlanovVV <vovancux39@gmail.com>
2024-10-30 13:38:57 +0200

    Update crud_functions.py

--- crud_functions.py
+++ crud_functions.py
@@ -21,6 +21,46 @@ def initiate_db():
     connection.commit()
     connection.close()
 
+    connection = sqlite3.connect('Users.db')
+    cursor = connection.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS Users(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT NOT NULL,
+        email TEXT NOT NULL,
+        age INTEGER NOT NULL,
+        balance INTEGER NOT NULL
+        )
+    ''')
+
+    connection.commit()
+    connection.close()
+
+
+def add_user(username, email, age, balance):
+    connection = sqlite3.connect('users.db')
+    cursor = connection.cursor()
+    cursor.execute('SELECT id, username, email, age, balance FROM Users')
+    if not is_included(username):
+        cursor.execute('INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)',
+                       (username, email, age, balance))
+
+    connection.commit()
+    connection.close()
+
+
+def is_included(username):
+    connection = sqlite3.connect('Users.db')
+    cursor = connection.cursor()
+    check_name = cursor.execute('SELECT * FROM Users WHERE username = ?', (username,))
+    try:
+        if check_name.fetchone()[1] == username:
+            return True
+    except TypeError:
+        return False
+    finally:
+        connection.close()
+
 
 def get_all_products():
     connection = sqlite3.connect('Products.db')
@@ -30,3 +70,4 @@ def get_all_products():
     connection.commit()
     connection.close()
     return list(db)
+
